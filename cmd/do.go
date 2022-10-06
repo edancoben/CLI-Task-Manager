46c9d5855613a98c91fa428a23e0251b2f8e7436
edan.coben <edan.coben@corteva.com>
2022-10-06 15:08:08 -0500

    get rid of uneeded prints

--- cmd/do.go
+++ cmd/do.go
@@ -23,7 +23,6 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("do called")
 		var task_ids []int
 		for _, value := range args {
 			i, err := strconv.Atoi(value)
